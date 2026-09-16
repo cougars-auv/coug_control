3655c1f92b4281251118112e87289aadb817da0d
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-16 15:05:47 -0600

    Add scenario param file

--- coug_control/launch/coug_control.launch.py
+++ coug_control/launch/coug_control.launch.py
@@ -39,6 +39,7 @@ def generate_launch_description() -> LaunchDescription:
             [agent_ns, "_params.yaml"],
         ]
     )
+    scenario_param_file = LaunchConfiguration("scenario_param_file")
 
     return LaunchDescription(
         [
@@ -50,6 +51,10 @@ def generate_launch_description() -> LaunchDescription:
                 "agent_ns",
                 default_value="auv0",
             ),
+            DeclareLaunchArgument(
+                "scenario_param_file",
+                default_value=agent_param_file,
+            ),
             Node(
                 package="coug_control",
                 executable="hsd_mode_converter",
@@ -57,6 +62,7 @@ def generate_launch_description() -> LaunchDescription:
                 parameters=[
                     fleet_param_file,
                     agent_param_file,
+                    scenario_param_file,
                     {
                         "use_sim_time": use_sim_time,
                         "map_frame": "map",
