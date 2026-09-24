fd88acdbb94366762c46cd699ff1918f7a2ef0dd
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-24 15:13:33 -0600

    Better launch file param handling

--- coug_control/launch/coug_control.launch.py
+++ coug_control/launch/coug_control.launch.py
@@ -29,15 +29,15 @@ def launch_setup(context: LaunchContext, *args: Any, **kwargs: Any) -> list[Acti
     use_sim_time = LaunchConfiguration("use_sim_time")
     agent_ns = LaunchConfiguration("agent_ns")
 
+    scenario_param_path = LaunchConfiguration("scenario_param_file").perform(context)
+
     fleet_param_file = PathJoinSubstitution(
         [EnvironmentVariable("CONFIG_DIR"), "fleet", "coug_control_params.yaml"]
     )
     agent_param_file = PathJoinSubstitution(
         [EnvironmentVariable("CONFIG_DIR"), [agent_ns, "_params.yaml"]]
     )
-    scenario_param_file = (
-        LaunchConfiguration("scenario_param_file").perform(context) or agent_param_file
-    )
+    scenario_param_file = scenario_param_path or agent_param_file
 
     return [
         Node(
